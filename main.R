0a57bf64f8ff04e56bda4240fb74b0e1e5fe8ac1
Victor M. Cortes Figueroa <vcortes@datuminference.com>
2021-01-27 18:48:34 -0600

    Tabla con "mini" graficas.
    Se agrega tabla además de que normaliza los nombres de los estados.

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 library(here)
 library(dplyr)
+library(kableExtra)
 
 
 data <- 
@@ -15,8 +16,47 @@ column_names[1] <- "estado"
 names(data) <- column_names
 
 # Eliminar primaras dos filas
-data <- data %>% slice(3:n())
+data <- 
+    data %>% 
+        slice(3:n()) %>%
+        mutate(
+            estado = iconv(estado, from = "LATIN1", to = "UTF-8")
+        )
+
+states <- data %>% pull(estado)
 
 # Reestructurar los datos con tidyr
-data %>% 
-    tidyr::pivot_longer(!estado, names_to = "year", values_to = "cantidad")
+long_data <- 
+    data %>% 
+        tidyr::pivot_longer(
+            !estado, 
+            names_to = "year", 
+            values_to = "cantidad"
+        )
+
+# Uso de kable extra para visualización
+by_states <- 
+    with(long_data, 
+        split(cantidad, estado)
+    )
+
+# "Layout" de la tabla
+inline_plot <- 
+    data.frame(
+        estados = states, 
+        boxplot = "",
+        line = ""
+    )
+
+inline_plot %>%
+    kbl() %>%
+    # kable_material(c("striped", "hover")) %>%
+    kable_paper(full_width = FALSE) %>%
+    column_spec(
+        column = 2, 
+        image = spec_boxplot(by_states, width = 300, file_type = "png"), 
+    ) %>%
+    column_spec(
+        column = 3, 
+        image = spec_plot(mpg_list, same_lim = TRUE, file_type = "png")
+    )
